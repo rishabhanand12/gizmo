0312b2407c8e44b5caa65e1e35e6daf6db1d6f40
Rishabh <mailrishabh.ra@gmail.com>
2020-06-20 21:52:42 +0530

    headernav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
 
 export default class Header extends React.Component {
-    
-    render() {
-        return (
-            <h1>Header</h1>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div class="container header">
+          <h1 class="logo">Gizmo</h1>
+          <form>
+            <input type="text" placeholder="Search"></input>
+            <select>
+              <option>EN</option>
+              <option>AR</option>
+              <option>DE</option>
+              <option>ES</option>
+              <option>FR</option>
+              <option>HE</option>
+              <option>IT</option>
+              <option>NL</option>
+              <option>NO</option>
+              <option>PT</option>
+              <option>RU</option>
+              <option>SE</option>
+              <option>UD</option>
+              <option>ZH</option>
+            </select>
+          </form>
+        </div>
+      </>
+    );
+  }
 }
